05495ac050f001a3ee303d5e6dd7d5e44a97d655
amandarover <rover.amanda@gmail.com>
2018-10-27 16:50:12 -0300

    Create endpoints for travels controller

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,2 +1,11 @@
 class TravelsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    render plain: params[:travel].inspect
+  end
 end
