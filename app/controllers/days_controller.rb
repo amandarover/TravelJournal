d237b71291b2a55b1b2c1324b0f1580469c49c5c
amandarover <rover.amanda@gmail.com>
2018-10-28 17:13:14 -0300

    Refactor day creation for travel

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -6,21 +6,34 @@ class DaysController < ApplicationController
     raise invalid_dates_error_message unless validate_travel_date(travel.init_date, travel.final_date)
 
     travel_days = []
-    travel_days_ordinal_count = ((travel.final_date - travel.init_date) / 1.day)
+    travel_duration_in_days = ((travel.final_date - travel.init_date) / 1.day)
     current_ordinal_day = 0
-    current_day = travel.init_date.to_date - 1.day
+    current_date = travel.init_date.to_date
 
-    while current_ordinal_day <= travel_days_ordinal_count
-      day_travel = Day.new
-      day_travel.date = (current_day += 1.day)
-      day_travel.travel_id = travel.id
-      day_travel.save
+    while current_ordinal_day <= travel_duration_in_days
+      day_travel = create_day(travel, current_date)
       travel_days << day_travel
       current_ordinal_day += 1
+      current_date += 1.day
     end
   end
 
+  def create_day(travel, current_date)
+    return false if day_already_exists(travel, current_date)
+
+    day_travel = Day.new
+    day_travel.date = current_date
+    day_travel.travel_id = travel.id
+    day_travel.save
+  end
+
   private
+
+  def day_already_exists(travel, current_date)
+    travel.days.each do |day|
+      return true if day.date == current_date
+    end
+  end
 
   def invalid_dates_error_message
     'The fisrt day should be minor than the last day'
