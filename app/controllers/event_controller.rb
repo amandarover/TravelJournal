9a6e12daa7ed99c5a3d2c0908d5197203b794dee
amandarover <rover.amanda@gmail.com>
2019-05-02 12:21:54 -0300

    Create method new and create on events_controller

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,46 @@
 class EventController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def new
+    @event = Event.new
+  end
+
+  def create(travel_id, day_id)
+    @event = Event.new(event_params)
+    if @event.save
+      DaysController.new.create_event_days(@event)
+      redirect_to event_path(@event.id)
+    else
+      logger.info("eventsController: Error to save event with params| #{event_params}")
+      render new_event_path
+    end
+  end
+
+  # def create_event(day)
+  #   raise invalid_dates_error_message unless validate_event_date(event.init_date, event.final_date)
+
+  #   event_events = []
+  #   event_duration_in_events = ((event.final_date - event.init_date) / 1.day) + 1
+  #   current_ordinal_day = 0
+  #   current_date = event.init_date.to_date
+
+  #   while current_ordinal_day < event_duration_in_events
+  #     day_event = create_day(event, current_date)
+  #     event_events << day_event
+  #     current_ordinal_day += 1
+  #     current_date += 1.day
+  #   end
+  # end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:type,
+                                  :name,
+                                  :address,
+                                  :starting_time,
+                                  :ending_time,
+                                  :pricing,
+                                  :description)
+  end
 end
