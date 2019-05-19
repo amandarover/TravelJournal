75831ca85005b6d0fed18d5d5bb1cb52434e32a7
amandarover <rover.amanda@gmail.com>
2019-05-19 12:42:00 -0300

    Create login local with google account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth) #take the data that Google returns and persist it to db
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
 
   get 'travels/index'
 
-  root 'travels#index'
+  # root 'travels#index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: 'home#show'
 
   resources :travels do
     resources :days do
