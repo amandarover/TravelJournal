f5a0fbff7c4847e211895e4a9b11c07864313b55
amandarover <rover.amanda@gmail.com>
2019-05-26 11:46:34 -0300

    Add client key on omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           ENV['GOOGLE_OAUTH2'],
+           ENV['GOOGLE_OAUTH2_CLIENT'],
+           ENV['GOOGLE_OAUTH2_KEY'],
            {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
